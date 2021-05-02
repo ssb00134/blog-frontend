eb990b350175a89f94ce336362c2869c5efef5a9
jun <ssb00134@naver.com>
2021-05-02 17:21:05 +0900

    프록시 설정

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -60,27 +60,51 @@ const textMap = {
   register : '회원가입'
 }
 
-const AuthForm = ({type}) => {
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
   const text = textMap[type];
-
-
-  return <AuthFormBlock><h3>{text}</h3>
-  <form>
-    <StyledInput autoComplete="username" name="username" placeholder='아이디' />
-    <StyledInput autoComplete="new-password" name="password" placeholder='비밀번호' type="password"/>
-    {type === 'register' &&(
-      <StyledInput autoComplete="new-password" name="passwordConfirm" placeholder="비밀번호 확인" type="password"/>
-    )}
-    <ButtonWithMarginTop cyan fullWidth>{text}</ButtonWithMarginTop>
-  </form>
-  <Footer>
-    {type==='login' ?(
-      <Link to="/register">회원가입</Link>
-    ):(
-      <Link to="/login">로그인</Link>
-    )}
-  </Footer>
-  </AuthFormBlock>;
+  return (
+    <AuthFormBlock>
+      <h3>{text}</h3>
+      <form onSubmit={onSubmit}>
+        <StyledInput
+          autoComplete="username"
+          name="username"
+          placeholder="아이디"
+          onChange={onChange}
+          value={form.username}
+        />
+        <StyledInput
+          autoComplete="new-password"
+          name="password"
+          placeholder="비밀번호"
+          type="password"
+          onChange={onChange}
+          value={form.password}
+        />
+        {type === 'register' && (
+          <StyledInput
+            autoComplete="new-password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            type="password"
+            onChange={onChange}
+            value={form.passwordConfirm}
+          />
+        )}
+       
+        <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
+          {text}
+        </ButtonWithMarginTop>
+      </form>
+      <Footer>
+        {type === 'login' ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
+      </Footer>
+    </AuthFormBlock>
+  );
 };
 
 export default AuthForm;
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,14 +1,56 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as authAPI from '../lib/api/auth';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+  'auth/REGISTER',
+);
 
-const initialState = {};
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+  'auth/LOGIN',
+);
+
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form,
+    key,
+    value,
+  }),
+);
+
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
+};
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value; // 예: state.register.username을 바꾼다
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+      authError: null, // 폼 전환 시 회원 인증 에러 초기화
+    }),
   },
   initialState,
 );
